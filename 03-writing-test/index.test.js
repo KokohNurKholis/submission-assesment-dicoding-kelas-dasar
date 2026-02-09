d9dd74b05a246939c32589d4a95cac9a18bdaa38
KokohNurKholis <kokohnur12@gmail.com>
2026-02-09 22:46:20 +0700

    update

---
+++ 03-writing-test/index.test.js
@@ -0,0 +1,22 @@
+import { test, expect } from 'bun:test';
+import { sum } from './sum.js';
+
+test('menjumlahkan bilangan positif', () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+test('menjumlahkan dengan nol', () => {
+  expect(sum(5, 0)).toBe(5);
+});
+
+test('menjumlahkan bilangan negatif', () => {
+  expect(sum(-2, -3)).toBe(-5);
+});
+
+test('menjumlahkan bilangan positif dan negatif', () => {
+  expect(sum(10, -4)).toBe(6);
+});
+
+test('menjumlahkan bilangan desimal', () => {
+  expect(sum(1.5, 2.5)).toBe(4);
+});
